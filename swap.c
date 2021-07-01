e4dfe64d5a2c8b581e3d46e9753e95da4fed3104
Keerthana1611 <84660994+Keerthana1611@users.noreply.github.com>
2021-07-01 19:49:49 +0530

    solution 4

--- swap.c
+++ swap.c
@@ -4,7 +4,7 @@
 void ssort(int arr[],int l)
 {
     int i;
-    for(i=0:i<l:i++)
+    for(i=0;i<l;i++)
     {
         swap_max(arr,l,i);
     }
@@ -14,7 +14,7 @@ void swap_max(int arr[],int l,int n)
 {
     int j,k;
     int temp;
-    for(j=k=n:j<l:j++)
+    for(j=k=n;j<l;j++)
     {
         if(arr[j]>arr[k])
         {
